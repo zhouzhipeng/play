393141d740e8ef9e270f03fa88990fd21a165ad2
zhouzhipeng <admin@zhouzhipeng.com>
2023-12-27 11:12:12 +0400

    update

--- server/build.rs
+++ server/build.rs
@@ -4,9 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::Write;
 use std::path::Path;
-//
-// use pyo3::{Py, PyAny, PyResult, Python};
-// use pyo3::prelude::PyModule;
+
 use serde_json::json;
 use walkdir::WalkDir;
 
@@ -27,8 +25,8 @@ fn main() {
     #[cfg(feature = "debug")]
     check_mod_files();
 
-    // #[cfg(feature = "debug")]
-    // gen_db_models_code();
+    #[cfg(feature = "debug")]
+    gen_db_models_code();
 
 
     //generate rustc args.
@@ -99,65 +97,70 @@ fn test() {
 }
 
 
-// fn gen_db_models_code() {
-//     let sql = include_str!("../doc/db_sqlite.sql");
-//     let table_info = parse_create_sql(sql);
-//     let ss = format!("table infos >> {:?}", table_info);
-//
-//     pyo3::prepare_freethreaded_python();
-//     let py_app = include_str!("python/simple_template.py");
-//     let model_template = include_str!("../doc/tmpl/model_template.rs.txt");
-//     let controller_template = include_str!("../doc/tmpl/controller_template.rs.txt");
-//
-//     Python::with_gil(|py| -> PyResult<Py<PyAny>> {
-//         // let syspath: &PyList = py.import("sys")?.getattr("path")?.downcast()?;
-//         // syspath.insert(0, &path)?;
-//         let render_fn: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
-//             .getattr("render_tpl_with_str_args")?
-//             .into();
-//         let set_debug_mode_fn: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
-//             .getattr("set_debug_mode")?
-//             .into();
-//         set_debug_mode_fn.call1(py, (true, ))?;
-//
-//         for info in table_info {
-//             let args = (model_template, "<tmp>", json!({"table_info": info}).to_string(), false);
-//             let model_content = render_fn.call1(py, args)?.to_string();
-//
-//
-//             let args = (controller_template, "<tmp>", json!({"table_name": info.table_name}).to_string(), false);
-//             let controller_content = render_fn.call1(py, args)?.to_string();
-//
-//             let dest_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/tables").join(format!("{}.rs", info.table_name));
-//             let controller_dest_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/controller").join(format!("{}_controller.rs", info.table_name));
-//             let mod_rs = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/tables/mod.rs");
-//             if !dest_file.exists() {
-//                 fs::write(&dest_file, model_content).expect(format!("create file failed! : {:?}", &dest_file).as_str());
-//
-//                 //add to mod.rs
-//                 let mut mod_rs_content = fs::read_to_string(&mod_rs).expect("read tables/mod.rs failed!");
-//                 if !mod_rs_content.contains(format!("mod {}", info.table_name).as_str()) {
-//                     mod_rs_content = format!("{}\npub mod {};", mod_rs_content, info.table_name);
-//                     fs::write(&mod_rs, mod_rs_content).expect("write tables/mod.rs failed");
-//                 }
-//             }
-//
-//             if !controller_dest_file.exists(){
-//                 fs::write(&controller_dest_file, controller_content).expect(format!("create file failed! : {:?}", &controller_dest_file).as_str());
-//             }
-//
-//             let template_dir  = Path::new(env!("CARGO_MANIFEST_DIR")).join(format!("templates/{}", info.table_name));
-//             if !template_dir.exists(){
-//                 fs::create_dir(&template_dir).expect(&format!("create dir :{:?} failed", template_dir));
-//                 let template = include_str!("../doc/tmpl/list_template.html.txt");
-//                 fs::write(template_dir.join("list.html"), template).expect("create list.html failed!");
-//
-//             }
-//
-//
-//
-//         }
-//
-//         Ok(render_fn)
-//     }).expect("run python error!");
-// }
+#[cfg(feature = "pyo3")]
+fn gen_db_models_code() {
+    //
+    use pyo3::{Py, PyAny, PyResult, Python};
+    use pyo3::prelude::PyModule;
+
+    let sql = include_str!("../doc/db_sqlite.sql");
+    let table_info = parse_create_sql(sql);
+    let ss = format!("table infos >> {:?}", table_info);
+
+    pyo3::prepare_freethreaded_python();
+    let py_app = include_str!("../libs/py_tpl/python/simple_template.py");
+    let model_template = include_str!("../doc/tmpl/model_template.rs.txt");
+    let controller_template = include_str!("../doc/tmpl/controller_template.rs.txt");
+
+    Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+        // let syspath: &PyList = py.import("sys")?.getattr("path")?.downcast()?;
+        // syspath.insert(0, &path)?;
+        let render_fn: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
+            .getattr("render_tpl_with_str_args")?
+            .into();
+        let set_debug_mode_fn: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
+            .getattr("set_debug_mode")?
+            .into();
+        set_debug_mode_fn.call1(py, (true, ))?;
+
+        for info in table_info {
+            let args = (model_template, "<tmp>", json!({"table_info": info}).to_string(), false);
+            let model_content = render_fn.call1(py, args)?.to_string();
+
+
+            let args = (controller_template, "<tmp>", json!({"table_name": info.table_name}).to_string(), false);
+            let controller_content = render_fn.call1(py, args)?.to_string();
+
+            let dest_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/tables").join(format!("{}.rs", info.table_name));
+            let controller_dest_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/controller").join(format!("{}_controller.rs", info.table_name));
+            let mod_rs = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/tables/mod.rs");
+            if !dest_file.exists() {
+                fs::write(&dest_file, model_content).expect(format!("create file failed! : {:?}", &dest_file).as_str());
+
+                //add to mod.rs
+                let mut mod_rs_content = fs::read_to_string(&mod_rs).expect("read tables/mod.rs failed!");
+                if !mod_rs_content.contains(format!("mod {}", info.table_name).as_str()) {
+                    mod_rs_content = format!("{}\npub mod {};", mod_rs_content, info.table_name);
+                    fs::write(&mod_rs, mod_rs_content).expect("write tables/mod.rs failed");
+                }
+            }
+
+            if !controller_dest_file.exists(){
+                fs::write(&controller_dest_file, controller_content).expect(format!("create file failed! : {:?}", &controller_dest_file).as_str());
+            }
+
+            let template_dir  = Path::new(env!("CARGO_MANIFEST_DIR")).join(format!("templates/{}", info.table_name));
+            if !template_dir.exists(){
+                fs::create_dir(&template_dir).expect(&format!("create dir :{:?} failed", template_dir));
+                let template = include_str!("../doc/tmpl/list_template.html.txt");
+                fs::write(template_dir.join("list.html"), template).expect("create list.html failed!");
+
+            }
+
+
+
+        }
+
+        Ok(render_fn)
+    }).expect("run python error!");
+}
