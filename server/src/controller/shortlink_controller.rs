824e259e096d71268bcaea1382ab82c830e7a464
zhouzhipeng <admin@zhouzhipeng.com>
2024-03-04 12:44:18 +0400

    add shortlink to config file

--- server/src/controller/shortlink_controller.rs
+++ server/src/controller/shortlink_controller.rs
@@ -1,10 +1,17 @@
-use crate::method_router;
+use axum::extract::Path;
+use crate::{method_router, S};
 use crate::R;
 
 method_router!(
     get : "/vpn"-> vpn,
+    get : "/s/:link"-> vpn,
 );
 async fn vpn() -> R<String> {
+    let res = reqwest::get("https://bit.ly/cmwowork").await?.text().await?;
+    Ok(res)
+}
+async fn link(s: S, name : Path<(String,)>) -> R<String> {
+    // s.config.finance
     let res = reqwest::get("https://bit.ly/cmwowork").await?.text().await?;
     Ok(res)
 }
