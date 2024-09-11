f0e9961d6d289f434ea30ec4b32459f6b0fe76c5
周志鹏 <823143047@qq.com>
2024-09-11 09:54:35 +0400

    fix path issue after re-organize project

--- crates/play_shared/src/lib.rs
+++ crates/play_shared/src/lib.rs
@@ -11,6 +11,8 @@ pub trait MyTrait{
     fn bark(&self)->String;
 }
 
+use std::path::PathBuf;
+use cargo_metadata::MetadataCommand;
 #[cfg(feature = "proc_macros")]
 pub use proc_macros::*;
 
@@ -35,4 +37,30 @@ macro_rules! timestamp_to_date_str {
         use chrono::TimeZone;
         chrono::Utc.timestamp_millis_opt($s).unwrap().format("%Y-%m-%d %H:%M:%S").to_string()
     }};
+}
+
+
+
+pub fn get_workspace_root() -> String {
+    let metadata = MetadataCommand::new()
+        .no_deps()
+        .exec()
+        .expect("Failed to get cargo metadata");
+
+    metadata.workspace_root.into_string()
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+
+    use super::*;
+
+    #[test]
+    fn test_parse() -> anyhow::Result<()> {
+        println!("{}", get_workspace_root());
+        Ok(())
+    }
 }
