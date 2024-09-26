4cc280c61401147fd925a225c2c2fd68297ce167
周志鹏 <823143047@qq.com>
2024-09-26 16:01:53 +0400

    add debug log

--- crates/play-dylib-example/src/lib.rs
+++ crates/play-dylib-example/src/lib.rs
@@ -3,33 +3,35 @@ use play_abi::http_abi::*;
 use play_abi::{async_request_handler, request_handler};
 
 // 异步处理函数
-async fn handle_request_impl(request: HttpRequest) -> anyhow::Result<HttpResponse> {
-    // 模拟一些异步操作
-    println!("Request handled request : {:?}", request);
-
-    let response = reqwest::get("https://crab.rs").await?.text().await?;
-    println!("response : {}", response);
-
-    Ok(HttpResponse{
-        headers: Default::default(),
-        body: "abcdefssss".to_string(),
-        status_code: 200,
-    })
-}
-
-async_request_handler!(handle_request_impl);
-// fn handle_request_impl(request: HttpRequest) -> anyhow::Result<HttpResponse> {
+// async fn handle_request_impl(request: HttpRequest) -> anyhow::Result<HttpResponse> {
 //     // 模拟一些异步操作
 //     println!("Request handled request : {:?}", request);
 //
-//     // let response = reqwest::get("https://crab.rs").await?.text().await?;
-//     // println!("response : {}", response);
+//     let response = reqwest::get("https://crab.rs").await?.text().await?;
+//     println!("response : {}", response);
 //
-//     Ok(HttpResponse {
+//     Ok(HttpResponse{
 //         headers: Default::default(),
-//         body: "sss".to_string(),
+//         body: "abcdefssss".to_string(),
 //         status_code: 200,
 //     })
 // }
 //
-// request_handler!(handle_request_impl);
+// async_request_handler!(handle_request_impl);
+
+
+fn handle_request_impl(request: HttpRequest) -> anyhow::Result<HttpResponse> {
+    // 模拟一些异步操作
+    println!("Request handled request : {:?}", request);
+
+    // let response = reqwest::get("https://crab.rs").await?.text().await?;
+    // println!("response : {}", response);
+
+    Ok(HttpResponse {
+        headers: Default::default(),
+        body: "sss".to_string(),
+        status_code: 200,
+    })
+}
+
+request_handler!(handle_request_impl);
