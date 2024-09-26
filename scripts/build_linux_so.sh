d69f16da36304285438f35d1a25e324adbb64de4
周志鹏 <823143047@qq.com>
2024-09-26 14:56:32 +0400

    add script

--- scripts/build_linux_so.sh
+++ scripts/build_linux_so.sh
@@ -2,7 +2,6 @@
 
 set  -eux
 
-cd ..
 docker build -f scripts/dockerfiles/build_linux_dylib_example.Dockerfile -t tmpimg .
 docker run --name tmpcontontainer tmpimg
-docker cp tmpcontontainer:/app/target/release/libplay_dylib_example.so . 
+docker cp tmpcontontainer:/app/target/release/libplay_dylib_example.so .
