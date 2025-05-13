f7db003df3268d0bebdbb13161bf929a03c8df69
周志鹏 <823143047@qq.com>
2025-05-13 15:05:17 +0400

    filter out static files from index page

--- server/src/controller/index_controller.rs
+++ server/src/controller/index_controller.rs
@@ -45,7 +45,11 @@ async fn root(s: S) -> HTML {
     let data = GeneralData::query_by_cat("title,url", "pages",1000, &s.db).await?;
     let pages = data.iter()
         .map(|p|serde_json::from_str::<PageDto>(&p.data).unwrap())
-        .filter(|p| !p.title.ends_with(".lua"))
+        .filter(|p| !p.title.contains(".lua")
+            &&  !p.url.ends_with(".js")
+            &&!p.url.ends_with(".json")
+            &&!p.url.ends_with(".css")
+        )
         .collect::<Vec<PageDto>>();
 
     let html = dioxus_ssr::render_element(rsx!{
